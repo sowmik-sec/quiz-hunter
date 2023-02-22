f9d72feb072a80647b409880a66ead8c9842d582
sowmik-sec <sowmik.sec@gmail.com>
2023-02-22 20:33:52 +0600

    react toast added

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
-const Options = ({ option }) => {
-  console.log(option);
+const Options = ({ option, correctAnswer }) => {
+  const handleChange = (e) => {
+    const userAns = e.target.value;
+    if (correctAnswer === userAns) {
+      toast.success("Correct ans");
+    } else {
+      toast.error("Wrong ans");
+    }
+  };
   return (
     <div className="border-2 rounded border-lime-500 m-2 p-2">
-      <input type="radio" name="option" id={option} value={option} />
-      <label className="ml-2" for={option}>
-        {option}
-      </label>
+      <input
+        type="radio"
+        name="option"
+        id={option}
+        value={option}
+        onChange={(e) => handleChange(e)}
+      />
+      <label className="ml-2">{option}</label>
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,9 +2,7 @@ import React from "react";
 import Options from "../Options/Options";
 
 const Quiz = ({ qn, count }) => {
-  const { question } = qn;
-  const { options } = qn;
-  console.log(options);
+  const { question, options, correctAnswer } = qn;
   return (
     <div className="md:w-[700px] md:mx-auto h-auto my-6 shadow-md p-2">
       <h3>
@@ -12,7 +10,7 @@ const Quiz = ({ qn, count }) => {
       </h3>
       <div className="grid md:grid-cols-2 text-left">
         {options.map((option) => (
-          <Options option={option} />
+          <Options option={option} correctAnswer={correctAnswer} key={option} />
         ))}
       </div>
     </div>
