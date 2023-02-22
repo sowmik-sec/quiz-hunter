231b1c1b181cada97d2a12661fce56505ad79e60
sowmik-sec <sowmik.sec@gmail.com>
2023-02-22 20:51:24 +0600

    eye icon added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,3 +1,5 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Options from "../Options/Options";
 
@@ -5,9 +7,12 @@ const Quiz = ({ qn, count }) => {
   const { question, options, correctAnswer } = qn;
   return (
     <div className="md:w-[700px] md:mx-auto h-auto my-6 shadow-md p-2">
-      <h3>
-        <span>Quiz {count}:</span> {question}
-      </h3>
+      <div className="flex justify-between items-center mb-8">
+        <h3 className="ml-12 w-[500px]">
+          <span>Quiz {count}:</span> {question}
+        </h3>
+        <FontAwesomeIcon icon={faEye} />
+      </div>
       <div className="grid md:grid-cols-2 text-left">
         {options.map((option) => (
           <Options option={option} correctAnswer={correctAnswer} key={option} />
