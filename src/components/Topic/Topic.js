68f66d7c02dd86f3751e79d2435fa0bbc1b241d5
sowmik-sec <sowmik.sec@gmail.com>
2023-02-22 15:51:25 +0600

    single topic style done

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -3,9 +3,17 @@ import React from "react";
 const Topic = ({ topic }) => {
   const { logo, name, total } = topic;
   return (
-    <div className="border-2 border-lime-500 m-1 p-3 w-60 bg-slate-300">
-      <img src={logo} alt="" />
-      <h3>{name}</h3>
+    <div className="shadow-md m-1 p-3 w-64 ">
+      <div className="bg-slate-300">
+        <img src={logo} alt="" />
+      </div>
+      <div className="flex justify-between pt-2">
+        <h3 className="text-lg font-bold">{name}</h3>
+        <p>Qn:{total}</p>
+        <button className="bg-blue-500 text-white px-2 py-1 rounded">
+          Start Practice
+        </button>
+      </div>
     </div>
   );
 };
