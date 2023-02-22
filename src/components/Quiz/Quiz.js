8631625917c95865c7d309bc77967b04764cdfdb
sowmik-sec <sowmik.sec@gmail.com>
2023-02-22 20:54:52 +0600

    eye button functionality added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,17 +1,21 @@
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { toast } from "react-hot-toast";
 import Options from "../Options/Options";
 
 const Quiz = ({ qn, count }) => {
   const { question, options, correctAnswer } = qn;
+  const handleEyeClick = () => {
+    toast.success(correctAnswer);
+  };
   return (
     <div className="md:w-[700px] md:mx-auto h-auto my-6 shadow-md p-2">
       <div className="flex justify-between items-center mb-8">
         <h3 className="ml-12 w-[500px]">
           <span>Quiz {count}:</span> {question}
         </h3>
-        <FontAwesomeIcon icon={faEye} />
+        <FontAwesomeIcon onClick={handleEyeClick} icon={faEye} />
       </div>
       <div className="grid md:grid-cols-2 text-left">
         {options.map((option) => (
